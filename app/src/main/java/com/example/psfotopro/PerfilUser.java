3dacd029b7839a552cb445b0e83d88735e70631b
auriopedrodev <auriopedro.ap@gmail.com>
2023-11-22 11:00:07 +0100

    FrontEdnd

---
+++ app/src/main/java/com/example/psfotopro/PerfilUser.java
@@ -0,0 +1,14 @@
+package com.example.psfotopro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_user);
+    }
+}
